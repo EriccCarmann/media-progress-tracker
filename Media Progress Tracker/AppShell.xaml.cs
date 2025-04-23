0b71846ef05c5a549810a8e595d12b47ac4bdcd5
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2025-04-23 15:08:33 +0300

    added project

---
+++ Media Progress Tracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Media_Progress_Tracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
